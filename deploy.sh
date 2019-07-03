847ee18d80af785e3afb6a48ac2e6cd05e996fd7
Thangadurai Nainamalai <duraithanga3@gmail.com>
2019-07-03 23:55:55 +0530

    Fix deploy.sh script

--- deploy.sh
+++ deploy.sh
@@ -22,10 +22,11 @@ rm -rf $PRESENT_WORKING_DIRECTORY/server/content
 mv $PRESENT_WORKING_DIRECTORY/client/build $PRESENT_WORKING_DIRECTORY/server/content
 
 # Zipping the bundle
-zip -r $ZIP_NAME content aws.js server.js package.json
+cd $PRESENT_WORKING_DIRECTORY/server
+zip -r $PRESENT_WORKING_DIRECTORY/$ZIP_NAME content aws.js server.js package.json
 
 # Upload to S3 for deployment to EB
-aws s3 cp $ZIP_NAME $S3_DEPLOY_URL
+aws s3 cp $PRESENT_WORKING_DIRECTORY/$ZIP_NAME $S3_DEPLOY_URL
 
 # Create application version label in EB
 aws elasticbeanstalk create-application-version --application-name $APPLICATION_NAME --version-label $VERSION_LABEL --description "AWS deployment of $ENVIRONMENT_NAME to $APPLICATION_NAME" --source-bundle S3Bucket="$S3_BUCKET_NAME",S3Key="$ZIP_NAME" --auto-create-application --region ap-south-1
@@ -33,4 +34,4 @@ aws elasticbeanstalk create-application-version --application-name $APPLICATION_
 # Deploy it in the environment
 aws elasticbeanstalk update-environment --environment-name $ENVIRONMENT_NAME --version-label $VERSION_LABEL --region ap-south-1
 
-echo 'Deployment to $ENVIRONMENT_NAME is completed!'
+echo "Deployment to $ENVIRONMENT_NAME is completed!"
